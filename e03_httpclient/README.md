17938036087e12e5a3c8e3ed87e1533fa7508f40
phucvin <phucvin@gmail.com>
2022-11-30 07:26:46 +0000

    add tokio flavor to fix error

--- e03_httpclient/README.md
+++ e03_httpclient/README.md
@@ -3,3 +3,11 @@ rustup target add wasm32-wasi
 cargo build --release --target wasm32-wasi
 
 cp target/wasm32-wasi/release/e03_httpclient.wasm httpclient.wasm
+
+# with t01
+
+wasmedge httpclient.wasm
+
+# with .cargo/config.toml
+
+cargo run
--- e03_httpclient/src/main.rs
+++ e03_httpclient/src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-#[tokio::main]
+#[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let resp = reqwest::get("http://eu.httpbin.org/ip")
         .await?
